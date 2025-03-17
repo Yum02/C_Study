53ad644e16fc9cd8e0187a30baebbd40c4a99dd1
0E <92708940+Yum02@users.noreply.github.com>
2025-03-18 00:07:32 +0900

    250317

---
+++ Chapter2/2-2/ex2-5.cpp
@@ -0,0 +1,11 @@
+// Description: 8진수와 16진수를 출력하는 프로그램
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", 12); // 10진수
+	printf("%d\n", 014); // 8진수
+	printf("%d\n", 0xc); // 16진수
+
+	return 0;
+}
