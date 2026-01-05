082fee05c220c70b5b113360569440a76ffe1fa3
0E <92708940+Yum02@users.noreply.github.com>
2026-01-05 22:39:20 +0900

    20260105

---
+++ Chapter7/7-2/ex7-2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int get_num(void);
+
+int main(void)
+{
+	int result;
+	
+	result = get_num();
+	printf("반환값 : %d\n", result);
+	return 0;
+}
+
+int get_num(void)
+{
+	int num;
+
+	printf("양수를 입력하세요 : ");
+	scanf("%d", &num);
+
+	return num;
+}
