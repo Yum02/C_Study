e97d9e9c44209097814ca5ff8e6149a33a81575c
0E <92708940+Yum02@users.noreply.github.com>
2026-01-07 22:39:48 +0900

    20260107

--- Chapter9/9-2/ex9-7.cpp
+++ Chapter9/9-2/ex9-7.cpp
@@ -1,6 +1,23 @@
+// 만약 포인터가 없으면 에러 발생
 #include<stdio.h>
+
+void swap(int* pa, int* pb);
 
 int main(void)
 {
+	int a = 10, b = 20;
 
+	swap(&a, &b);
+	printf("a : %d, b : %d\n", a, b);
+
+	return 0;
+}
+
+void swap(int* pa, int* pb)
+{
+	int temp;
+
+	temp = *pa; //temp에 pa가 가리키는 변수 값 저장
+	*pa = *pb; //pa가 가리키는 변수에 pb가 가리키는 변수 값 저장
+	*pb = temp; //pb가 가리키는 변수에 temp값 저장
 }
