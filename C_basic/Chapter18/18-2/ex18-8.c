08ad1fd7fd3a6ec263c5e98db2cde55e9df8590b
0E <92708940+Yum02@users.noreply.github.com>
2026-01-14 22:48:30 +0900

    .

--- C_basic/Chapter18/18-2/ex18-8.c
+++ C_basic/Chapter18/18-2/ex18-8.c
@@ -1,2 +1,41 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	FILE* ifp, * ofp;
+	char str[80];
+	char* res;
+
+	ifp = fopen("a.txt", "r");
+	if (ifp == NULL)
+	{
+		printf("Input file open error!\n");
+		return 1;
+	}
+
+	ofp = fopen("b.txt", "w");
+	if (ofp == NULL)
+	{
+		printf("Output file open error!\n");
+		return 1;
+	}
+
+	while (1)
+	{
+		res = fgets(str, sizeof(str), ifp);
+		if (res == NULL)
+		{
+			break;
+		}
+		str[strlen(str) - 1] = '\0';
+		fputs(str, ofp);
+		fputs(" ", ofp);
+	}
+
+	fclose(ifp);
+	fclose(ofp);
+
+	return 0;
+}
